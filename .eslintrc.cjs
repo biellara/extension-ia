b1116a21e32e155b4e2641568c0e2d9c7d2480cf
biellaraa <gdias1414@gmail.com>
2025-08-20 12:08:18 -0300

    fix: fix errors from lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,12 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true, node: true },
+  env: { browser: true, es2020: true }, // Ambiente padrão para o código-fonte
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist'], // Não é mais necessário ignorar o próprio arquivo de config
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
@@ -14,7 +14,6 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    // Adiciona a regra para ignorar variáveis não utilizadas que começam com _
     '@typescript-eslint/no-unused-vars': [
       'error',
       {
@@ -24,4 +23,13 @@ module.exports = {
       },
     ],
   },
+  // Adiciona uma seção de "overrides" para o próprio arquivo de configuração
+  overrides: [
+    {
+      files: ['.eslintrc.cjs'],
+      env: {
+        node: true, // Habilita o ambiente Node.js apenas para este arquivo
+      },
+    },
+  ],
 };
