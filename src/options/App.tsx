7de720e2074456cd51b25147307c0196e50334f9
biellaraa <gdias1414@gmail.com>
2025-08-20 12:26:17 -0300

    fix: fix errors from lint

--- src/options/App.tsx
+++ src/options/App.tsx
@@ -1,7 +1,6 @@
 // switches: Captura automática ON/OFF, Anonimização ON, Retenção (7/15/30), Limite (500/1000/2000).
 // lê/grava no storage (através do wrapper em /common/storage).
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React from 'react';
 
 export default function App() {
