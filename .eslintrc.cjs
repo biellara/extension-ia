5176b84b23d902c6d46a9b2852bb71024e879ce0
biellaraa <gdias1414@gmail.com>
2025-08-20 12:21:37 -0300

    fix: fix errors from lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true }, // Ambiente padrão para o código-fonte
+  env: { browser: true, es2020: true, node: true }, // Ambiente padrão para o código-fonte
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
